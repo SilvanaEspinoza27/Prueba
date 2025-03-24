e585b17de25161c5459107a2e34a0a01f704a2ba
Silvana <sespinoza@uteq.edu.ec>
2025-03-24 11:42:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/prueba/clases/Journal.java
@@ -0,0 +1,49 @@
+package com.example.prueba.clases;
+
+public class Journal {
+    private String journalId;
+    private String portada;
+    private String abbreviation;
+    private String description;
+    private String name;
+
+    public String getJournalId() {
+        return journalId;
+    }
+
+    public void setJournalId(String journalId) {
+        this.journalId = journalId;
+    }
+
+    public String getPortada() {
+        return portada;
+    }
+
+    public void setPortada(String portada) {
+        this.portada = portada;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
